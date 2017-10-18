2f5655eae4a23d031955eb64e59be7e138188947
James <jmcadoo89@gmail.com>
2017-10-18 09:32:06 -0500

    pretty much finished

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 
+// Global variables
 
+var kirbyDiv = document.getElementById('kirby');
+var poppyDiv = document.getElementById('poppy');
+var posK;
+var posP;
+var kirbyImage = document.getElementById('kirbyImg');
+var poppyImage = document.getElementById('poppyImg');
 
-var k = document.getElementById('kirby');
-var p = document.getElementById('poppy');
+// Resets image sources and position
+function setUp(){
+  posK = 0;
+  posP = 0;
+  kirbyDiv.style.left = posK + 'px';
+  poppyDiv.style.left = posP + 'px';
+  kirbyImage.src = "kirby_bomb.png";
+  poppyImage.src = "poppy_bomb.png";
+  kirbyImage.style.display = "block";
+  poppyImage.style.display = "block";
+  document.getElementById('kirbyWinsImg').style.display = "none";
+  document.getElementById('poppyWinsImg').style.display = "none";
+  document.getElementById('startButton').style.display = "block";
+  document.getElementById('startButtonBW').style.display = "none";
+}
 
-
-
+// Conduct "the race"
 function race() {
-  var posK = 0;
-  var posP = 0;
-  resetImages();
+  setUp();
+  document.getElementById('startButton').style.display = "none";
+  document.getElementById('startButtonBW').style.display = "block";
+  // The frame() function occurs at interval
   var timer = setInterval(frame, 100);
-
+// Moves the images to the right by a random number between 1 and 20
   function frame (){
     if (posK > 500 || posP > 500 ){
       clearInterval(timer);
@@ -21,20 +41,21 @@ function race() {
       var rdmP = Math.floor(Math.random() * 20);
       posK += rdmK;
       posP += rdmP;
-      k.style.left = posK + 'px';
-      p.style.left = posP + 'px';
+      kirbyDiv.style.left = posK + 'px';
+      poppyDiv.style.left = posP + 'px';
 
     }
   }
 }
-function resetImages() {
-  document.getElementById('kirbyImg').src = "kirby_bomb.png";
-  document.getElementById('poppyImg').src = "poppy_bomb.png";
-}
+
+// Makes the winning image visible & hides race image
 function kirbyWins(){
-  document.getElementById('kirbyImg').src = "kirby_wins.png";
-  alert('kirby');
+  kirbyImage.style.display = "none";
+  document.getElementById('kirbyWinsImg').style.display = "block";
+
 }
+// Makes the winning image visible & hides race image
 function poppyWins(){
-  document.getElementById('poppyImg').src = "poppy_wins.png";
+  poppyImage.style.display = "none";
+  document.getElementById('poppyWinsImg').style.display = "block";
 }
