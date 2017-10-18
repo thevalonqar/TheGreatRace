fda32a453823622ce7d1b4ae91b11b1a6dcc841b
James <jmcadoo89@gmail.com>
2017-10-18 07:03:12 -0500

    functions to change image on win

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ var p = document.getElementById('poppy');
 function race() {
   var posK = 0;
   var posP = 0;
+  resetImages();
   var timer = setInterval(frame, 100);
 
   function frame (){
     if (posK > 500 || posP > 500 ){
       clearInterval(timer);
-      posK > posP ? alert('kirby') : alert('poppy');
+      posK > posP ? kirbyWins() : poppyWins();
     } else {
       var rdmK = Math.floor(Math.random() * 20);
       var rdmP = Math.floor(Math.random() * 20);
@@ -25,4 +26,15 @@ function race() {
 
     }
   }
+}
+function resetImages() {
+  document.getElementById('kirbyImg').src = "kirby_bomb.png";
+  document.getElementById('poppyImg').src = "poppy_bomb.png";
+}
+function kirbyWins(){
+  document.getElementById('kirbyImg').src = "kirby_wins.png";
+  alert('kirby');
+}
+function poppyWins(){
+  document.getElementById('poppyImg').src = "poppy_wins.png";
 }
